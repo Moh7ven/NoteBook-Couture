3f95b93022c0e2778dfd46ec9f9352b2b49c739f
Moh7ven <Mohamedsangare0107@gmail.com>
2023-09-28 04:10:45 +0000

    auth

--- js/connexion.js
+++ js/connexion.js
@@ -1,29 +1,46 @@
 let btnConn = document.querySelector("#conn");
+let email = document.querySelector("#email").value;
+let mdp = document.querySelector("#password").value;
+
+function validateForm() {
+  if (email === "") {
+    alert("Veuillez entrer votre Email");
+    return false;
+  }
+
+  if (mdp === "") {
+    alert("Veuillez entrer votre mot de passe");
+    return false;
+  }
+  return true;
+}
 
 function connect() {
-  let email = document.querySelector("#email").value;
-  let mdp = document.querySelector("#password").value;
+  if (validateForm() === true) {
+    let email = document.querySelector("#email").value;
+    let mdp = document.querySelector("#password").value;
 
-  let bdCouturier = JSON.parse(localStorage.getItem("couturier")) || [];
-  console.log(bdCouturier);
-  let result;
+    let bdCouturier = JSON.parse(localStorage.getItem("couturier")) || [];
+    console.log(bdCouturier);
+    let result;
 
-  bdCouturier.find((element) => {
-    if (
-      email === element.emailCouturier &&
-      mdp === element.motDePasseCouturier
-    ) {
-      result = element;
-      console.log(result);
-    }
-    if (typeof result !== "undefined") {
-      sessionStorage.setItem("session", JSON.stringify(result));
-      window.location.replace("./dashboard.html");
-    } else {
-      alert("Utilisateur inexistant !");
-      window.location.reload();
-    }
-  });
+    bdCouturier.find((element) => {
+      if (
+        email === element.emailCouturier &&
+        mdp === element.motDePasseCouturier
+      ) {
+        result = element;
+        console.log(result);
+      }
+      if (typeof result !== "undefined") {
+        sessionStorage.setItem("session", JSON.stringify(result));
+        window.location.replace("./dashboard.html");
+      } else {
+        alert("Utilisateur inexistant !");
+        window.location.reload();
+      }
+    });
+  }
 }
 
 // connect();
